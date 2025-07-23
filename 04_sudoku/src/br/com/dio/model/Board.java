f2e4cbeaab98a879c6a98d2183acdb9e39169c6f
Lucas Matheus <99808167+LucasZerino@users.noreply.github.com>
2025-07-23 16:38:04 -0300

    Insere metodos para alterar valores no quadro e validar o status do jogo

--- 04_sudoku/src/br/com/dio/model/Board.java
+++ 04_sudoku/src/br/com/dio/model/Board.java
@@ -1,6 +1,11 @@
 package br.com.dio.model;
 
+import java.util.Collection;
 import java.util.List;
+
+import static br.com.dio.model.GameStatusEnum.*;
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 public class Board {
 
@@ -12,6 +17,51 @@ public class Board {
 
     public List<List<Space>> getSpaces() {
         return spaces;
+    }
+
+    public GameStatusEnum getStatus(){
+        // Verifica se a lista tem posições não fixas que não são nullas ( não temos espaços preenchidos pelo usuário )
+        if(spaces.stream().flatMap(Collection::stream).noneMatch(s -> !s.isFixed() && nonNull(s.getActual()))){
+            return NON_STARTED;
+        }
+        // Verifica se ainda temos espaços em branco ou se está completo
+        return spaces.stream().flatMap(Collection::stream).anyMatch(s-> isNull(s.getActual())) ? INCOMPLETE : COMPLETE;
+    }
+
+    public boolean hasErrors(){
+        if(getStatus() == NON_STARTED){
+            return false;
+        }
+        //Valida se algum espaço que tem a propriedade atual preenchida, é diferente da propriedade expected
+        return spaces.stream().flatMap(Collection::stream)
+                .anyMatch(s -> nonNull(s.getActual()) && !s.getActual().equals(s.getExpected()));
+
+    }
+
+    public boolean changeValue(final int col, final int row, final int value){
+        var space = spaces.get(col).get(row);
+        if(space.isFixed()) return false;
+
+        space.setActual(value);
+        return true;
+    }
+
+    public boolean clearValue(final int col, final int row){
+        var space = spaces.get(col).get(row);
+                if(space.isFixed()){
+                    return false;
+                }
+                space.clearSpace();
+                return true;
+
+    }
+
+    public void reset(){
+        spaces.forEach(c -> c.forEach(Space::clearSpace));
+    }
+
+    public boolean gameIsFinished(){
+        return !hasErrors() && getStatus().equals(COMPLETE);
     }
 
 }
