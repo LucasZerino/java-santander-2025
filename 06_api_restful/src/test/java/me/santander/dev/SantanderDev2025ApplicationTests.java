62ad01c4ac58c6910fdf56eff2dafd658eb9e5d3
Lucas Matheus <99808167+LucasZerino@users.noreply.github.com>
2025-08-02 10:54:41 -0300

    Inicio do projeto
    - iniciado com start.pring.io

---
+++ 06_api_restful/src/test/java/me/santander/dev/SantanderDev2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.santander.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDev2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
