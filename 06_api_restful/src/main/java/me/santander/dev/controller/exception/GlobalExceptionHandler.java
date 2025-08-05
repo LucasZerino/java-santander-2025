ed357665b3eb6e073061a7a9c70dd7f165922d63
Lucas Matheus <99808167+LucasZerino@users.noreply.github.com>
2025-08-05 09:39:08 -0300

    Fix: Throwable class

--- 06_api_restful/src/main/java/me/santander/dev/controller/exception/GlobalExceptionHandler.java
+++ 06_api_restful/src/main/java/me/santander/dev/controller/exception/GlobalExceptionHandler.java
@@ -25,7 +25,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Throwable.class)
-    public ResponseEntity<String> handleUnexpectedException(NoSuchElementException unexpectedException){
+    public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException){
         var message = "Unexpected server error, see the logs.";
         logger.error(message, unexpectedException);
         return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
